14c0e51e62535a1984e697cccf422ec9a7bfd308
Hao Le <n01605830@humbermail.ca>
2024-05-30 23:30:27 -0400

    Update 2

--- src/LabAssignment.java
+++ src/LabAssignment.java
@@ -1,51 +1,55 @@
 public class LabAssignment {
+    // instance variables
     private int numb;
     private double decimal;
     private boolean bool;
     private String str;
-public LabAssignment(int numb, double decimal, boolean bool, String str)
-{
-    this.numb = numb;
-    this.decimal = decimal;
-    this.bool = bool;
-    this.str = str;
-}
-public void printVar(){
-    System.out.println(numb);
-    System.out.println(decimal);
-    System.out.println(bool);
-    System.out.println(str);
-}
-public void checkNumber(int numb){
-    if (numb > 0){
-        System.out.println("The number is positive");
+    // constructor
+    public LabAssignment(int numb, double decimal, boolean bool, String str) {
+        this.numb = numb;
+        this.decimal = decimal;
+        this.bool = bool;
+        this.str = str;
     }
-    else if (numb < 0){
-        System.out.println("The number is negative");
+    // print the variables
+    public void printVar() {
+        System.out.println(numb);
+        System.out.println(decimal);
+        System.out.println(bool);
+        System.out.println(str);
     }
-    else {
-        System.out.println("The number is zero");
-    }
-}
-public void printNumbers(int numb){
-    for (int i = 1; i <= numb; i++){
-        System.out.println(i);
-    }
-}
-public void calculateAverage(int[] arr){
-    double sum = 0;
-    for (int i = 0; i < arr.length; i++){
-        sum += arr[i];
-    }
-    double average = sum/arr.length;
-    System.out.println(average);
-}
-public boolean isPrime(int numb){
-    for (int i = 2; i < numb; i++){
-        if (numb % i == 0){
-            return false;
+    // check if the number is positive, negative or zero
+    public void checkNumber(int numb) {
+        if (numb > 0) {
+            System.out.println("The number is positive");
+        } else if (numb < 0) {
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The number is zero");
         }
     }
-    return true;
-}
+    // print numbers from 1 to numb
+    public void printNumbers(int numb) {
+        for (int i = 1; i <= numb; i++) {
+            System.out.println(i);
+        }
+    }
+    // calculate the average of an array of integers
+    public void calculateAverage(int[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double average = sum / arr.length;
+        System.out.println(average);
+    }
+    // check if a number is prime
+    public boolean isPrime(int numb) {
+        for (int i = 2; i < numb; i++) {
+            if (numb % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        // create an object of LabAssignment
         LabAssignment myAssignment = new LabAssignment(2, 3.5, true, "Hello Mr.Gabriel");
+        // call all method
         myAssignment.printVar();
-
         myAssignment.checkNumber(-2);
         myAssignment.printNumbers(5);
 
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = { 1, 2, 3, 4, 5 };
         myAssignment.calculateAverage(arr);
 
         System.out.println(myAssignment.isPrime(11));
